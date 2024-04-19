66b3be6e96d8e01c3ea1fa26ffe6380d679312fa
Louis Bompart <lbompart@coveo.com>
2024-04-19 10:38:09 -0400

    test1

---
+++ index.js
@@ -0,0 +1,18 @@
+import rehypeSanitize from "rehype-sanitize";
+import rehypeStringify from "rehype-stringify";
+import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
+
+const rawAnswer = "Hello, **world**!";
+
+const answer = String(
+  unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeSanitize)
+    .use(rehypeStringify)
+    .processSync(rawAnswer)
+);
